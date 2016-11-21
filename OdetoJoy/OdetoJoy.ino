a66f4f343fabf59df8b0084cf1fcfc5ba8fff628
Lei <ivy99917@gmail.com>
2016-11-21 13:16:23 +0800

    OdetoJoy update

--- OdetoJoy/OdetoJoy.ino
+++ OdetoJoy/OdetoJoy.ino
@@ -1,33 +1,17 @@
-int buzzerPin = 8;
 int note[]={330,330,349,392,392,349,330,294,262,262,294,330,330,294,294,330,330,349,392,392,349,330,294,262,262,294,330,294,262,262}; //樂譜
+float beat[]={1,1,1,1,1,1,1,1,1,1,1,1,1.5,0.5,2,1,1,1,1,1,1,1,1,1,1,1,1,1.5,0.5,2}; //節拍
 int lenghofnote = sizeof(note)/sizeof(note[0]);
+float bt=0;
 
 void setup() {
-  pinMode(buzzerPin,OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   for(int i=0;i<lenghofnote;i++){
-    if(i==13 || i==28){ //八分音符
-      tone(buzzerPin,note[i],250);
-      delay(250);
-    }
-    else if(i==14 || i==29){ //二分音符
-      tone(buzzerPin,note[i],1000);
-      delay(1000);
-    }
-    else if(i==12 || i==27){ //附點四分音符
-      tone(buzzerPin,note[i],750);
-      delay(750);
-    }
-    else if(i==13){ 
-      delay(250);
-      tone(buzzerPin,note[i],500);
-      delay(500);
-    }
-    else{ //四分音符
-      tone(buzzerPin,note[i],500);
-      delay(500);
-    }
+//    bt=500*beat[i];
+//    tone(8,note[i],bt);
+//    delay(bt);
+//    Serial.println(bt);
   }
 }
