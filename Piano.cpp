d6edf267c59fbb2a68647e9ecdf1e705704068ee
Lei <ivy99917@gmail.com>
2016-11-29 23:44:27 +0800

    Piano library update

--- Piano.cpp
+++ Piano.cpp
@@ -4,16 +4,19 @@
 Piano::Piano(){
 }
 
+//設定buzzer
 void Piano::setBuzzer(int buzzerpin){
 	_buzzerpin = buzzerpin;
 	pinMode(_buzzerpin,OUTPUT);
 }
 
+//取得循線感測器數值
 int Piano::getIRVal(int irpin){
 	int irval = digitalRead(irpin);
 	return(irval);
 }
 
+//設定循線感測器琴鍵音高
 void Piano::setPitch(int irpin,int ledpin,int pitch,int delaytime){
 	pinMode(irpin,INPUT);
 	pinMode(ledpin,OUTPUT);
@@ -28,27 +31,32 @@ void Piano::setPitch(int irpin,int ledpin,int pitch,int delaytime){
 	}
 }
 
-void Piano::MusicTutorial(int irpin[],int ledpin[],int pitches[],int notes[],int beat[],int numofpitches,int numofnotes){
-	int irval[numofnotes];
+//樂曲教學模式
+void Piano::MusicTutorial(int irpin[],int ledpin[],int pitches[],int notes[],float beat[],int numofpitches,int numofnotes){
+	int irval[numofnotes]; 
 
 	for(int i=0;i<numofpitches;i++){
 		pinMode(irpin[i],INPUT);
 		pinMode(ledpin[i],OUTPUT);
 	}
+
 	for(int i=0;i<numofnotes;i++){
 		digitalWrite(ledpin[notes[i]],HIGH);
 		irval[i]=digitalRead(irpin[notes[i]]);
 
 		if(irval[i]==LOW){
 			digitalWrite(ledpin[notes[i]],LOW);
-			digitalWrite(ledpin[notes[i]+1],HIGH);
 			tone(_buzzerpin,pitches[notes[i]],beat[i]*500);
+			delay(200);
+			digitalWrite(ledpin[notes[i+1]],HIGH);
 		}
 		else{
 			i--;
 		}
+		delay(100);
 	}
 }
+
 
 
 
--- Piano.h
+++ Piano.h
@@ -5,10 +5,10 @@
 class Piano{
 public:
 	Piano();
+	int getIRVal(int irpin);
 	void setBuzzer(int buzzerpin);
 	void setPitch(int irpin,int ledpin,int pitch,int delaytime);
-	void MusicTutorial(int irpin[],int ledpin[],int pitches[],int notes[],int beat[],int numofpitches,int numofnotes);
-	int getIRVal(int irpin);
+	void MusicTutorial(int irpin[],int ledpin[],int pitches[],int notes[],float beat[],int numofpitches,int numofnotes);
 private:
 	int _buzzerpin;
 };
